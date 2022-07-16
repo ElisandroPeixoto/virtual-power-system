8a11d1c717babd6619e56a6cdf1140457557f26f
Elisandro Peixoto <elisandropeixoto21@gmail.com>
2022-07-16 18:05:39 -0300

    inicio pagina AL-1

--- subestacao/urls.py
+++ subestacao/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import subestacao_simulacao
+from .views import subestacao_simulacao, al1
 
 urlpatterns = [
     path('subestacao_simulacao/', subestacao_simulacao, name='subestacao_simulacao'),
+    path('subestacao_simulacao/AL1', al1, name='al1'),
 ]
--- subestacao/views.py
+++ subestacao/views.py
@@ -5,3 +5,8 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def subestacao_simulacao(request):
     return render(request, 'subestacao_simu.html')
+
+
+# @login_required
+def al1(request):
+    return render(request, 'al1_simu.html')
